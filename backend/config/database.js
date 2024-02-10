3992f73156c534587c0946c828b82933f7e1807c
AbhishekBhure <abhiab7411@gmail.com>
2024-02-10 12:58:03 +0530

    modified:added mongo atlas url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex:true
-    })
-    .then((data) => {
-      console.log(`mongodb connected with server:${data.connection.host}`);
-    });
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log(`mongodb connected with server:${data.connection.host}`);
+  });
 };
 
 module.exports = connectDatabase;
